fcd1a9e4299b42721aa62d48ae7b39e90754481b
doeig <dominik.eigner@gmx.de>
2023-06-23 17:15:43 +0200

    first look

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		DarkMode,
+		Footer,
+		FooterLinkGroup,
+		FooterLink
+	} from 'flowbite-svelte';
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<img src="/KLJB-Logo-cmyk.png" class="mr-3 h-6 md:h-9" alt="KLJB Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			KLJB Kläham-Oberergoldsbach
+		</span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+
+	<NavUl {hidden}>
+		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi href="/about">Mitglied werden</NavLi>
+	</NavUl>
+
+	<DarkMode size="sm" />
+</Navbar>
+
+<Footer class="absolute bottom-0 left-0 z-20 w-full">
+	<FooterLinkGroup
+		ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+	>
+		<FooterLink href="/">Impressum</FooterLink>
+		<FooterLink href="/">Datenschutz</FooterLink>
+	</FooterLinkGroup>
+</Footer>
