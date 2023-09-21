dc01f15a50edddc1ed347bede50f980e19632097
doeig <dominik.eigner@gmx.de>
2023-09-21 22:04:01 +0200

    update packages, used layout page and added styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,15 @@
 <script>
 </script>
 
-<section class="bg-white dark:bg-gray-900">
-	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-		<div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-			<h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-700 dark:text-white">
-				Neuigkeiten
-			</h2>
-			<p class="mb-4 font-light">
-				Track work across the enterprise through an open, collaborative platform. Link issues across
-				Jira and ingest data from other software development tools, so your IT support and
-				operations teams have richer contextual information to rapidly respond to requests,
-				incidents, and changes.
-			</p>
-			<p class="mb-4 font-medium">
-				Deliver great service experiences fast - without the complexity of traditional ITSM
-				solutions.Accelerate critical development work, eliminate toil, and deploy changes with
-				ease.
-			</p>
-		</div>
-	</div>
-</section>
+<div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
+	<h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-700 dark:text-white">Neuigkeiten</h2>
+	<p class="mb-4 font-light">
+		Track work across the enterprise through an open, collaborative platform. Link issues across
+		Jira and ingest data from other software development tools, so your IT support and operations
+		teams have richer contextual information to rapidly respond to requests, incidents, and changes.
+	</p>
+	<p class="mb-4 font-medium">
+		Deliver great service experiences fast - without the complexity of traditional ITSM
+		solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease.
+	</p>
+</div>
